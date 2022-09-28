007a8f420e5fbd83152db5b66ee4af6e6b38372f
Xgabi86 <gabiche.86@gmail.com>
2022-09-28 14:37:06 +0200

    c

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-a = "abcd"
+import random
+
+print("Voulez vous miner ? [Oui/Non]")
+minea = input()
+if minea == "Oui":
+    charbonT = (random.randrange(1, 3))
+    charbon = str(charbonT)
+    print("Vous avez miner", charbon, "charbon.")
+elif minea == "Non":
+    print("N")
 with open('DB1.txt', 'w') as writer:
-    writer.write(a)
+    writer.write(charbon)
     print("DB1 Set to:")
-    print(a)
+    print(charbon)
