bdd93978e22d239d33008c361b8065fdc5b863a7
Xgabi86 <gabiche.86@gmail.com>
2022-09-29 21:44:10 +0200

    e

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import random, os, codecs
 
 print("Voulez vous miner ?")
-rep1 = input()
+rep1 = input("Rep: ")
 if rep1 == "":
 
+    print("E1")
     charbonT = (random.randrange(1, 3))
     fd = "DB1.txt"
     file = open(fd, 'r')
@@ -15,6 +16,11 @@ if rep1 == "":
     with open('DB1.txt', 'w') as writer:
         writer.write(charbon)
         os.close(2)
+    
+    del(rep1)
+    del(charbonT)
+    del(charbonS)
+    del(charbon)
 
     os.system("main.py")
 
